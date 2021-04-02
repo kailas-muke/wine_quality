1a1253b79bfa07215627c1d27fc0a45db6e9755f
Kailas <kailasmuke37@gmail.com>
2021-04-02 23:11:55 +0530

    update prediction service

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 import yaml
 import joblib
 import numpy as np
+from prediction_service import prediction
 
-params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -12,51 +12,24 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try again"}
-        return error
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                #data = [list(map(float, data))]
-                data = [list(map(lambda x: x[0], data))]
-                #data = [list(float(s) for s in data)]
-                #data = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
-                response = predict(data)
+                data_req = dict(request.form)
+                response = prediction.form_respone(data_req)
                 return render_template("index.html", response=response)
 
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
             error = {"error": "Something wrong!! Try again"}
-            return render_template("404.html", error=error)
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
