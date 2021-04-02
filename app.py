fd3bc6118a6b6980d230681d74737b01a73193e7
Kailas <kailasmuke37@gmail.com>
2021-04-03 00:18:51 +0530

    update app and tested with postman

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def index():
 
         except Exception as e:
             print(e)
-            error = {"error": "main Something wrong!! Try again later"}
-            #error = {"error": e}
+            #error = {"error": "main Something wrong!! Try again later"}
+            error = {"error": e}
 
             return render_template("404.html", error=error)
     else:
